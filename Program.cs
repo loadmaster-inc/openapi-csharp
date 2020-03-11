a2204a889441f63ce340cacb7cba4e135cc0a5eb
david <david@zhuangxiang.com>
2020-03-11 15:50:12 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoadMaster_api_call_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
